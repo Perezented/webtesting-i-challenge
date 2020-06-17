da58538aba241a19c36f1efef7eaef838905fab1
Michael Perez <mperez6473@gmail.com>
2020-06-17 14:41:20 -0500

    Started a table for items

--- data/migrations/20200617143353_init.js
+++ data/migrations/20200617143353_init.js
@@ -1,8 +1,16 @@
+const { toBlock } = require("@babel/types");
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable(
+        "Items"((tbl) => {
+            tbl.increments();
+            tbl.string("name").notNullable().unique().index();
+            tbl.integer("enhancement");
+            tbl.integer("durability");
+        })
+    );
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists("items");
 };
