a9da5ed92b62c5bbf6242f43e01792eca1f8926e
Michael Perez <mperez6473@gmail.com>
2020-06-17 17:05:16 -0500

    Added a jest file and started running some tests. Having an issue at the moment. seems to be the knex file.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,10 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
+const { describe } = require("yargs");
+const { italic } = require("chalk");
+const db = require("../data/connection.js");
 // test away!
+describe("repairs item", function () {
+    it("should repair an item to 100", function () {
+        expect(true).toBe(true);
+    });
+});
